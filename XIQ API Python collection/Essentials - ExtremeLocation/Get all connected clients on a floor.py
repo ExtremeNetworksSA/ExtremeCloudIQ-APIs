896b3874e202286af8f210d8d00321bd68338330
timjsmith24 <20387652+timjsmith24@users.noreply.github.com>
2026-06-01 10:17:00 -0400

    25r10 updates

---
+++ XIQ API Python collection/Essentials - ExtremeLocation/Get all connected clients on a floor.py
@@ -0,0 +1,26 @@
+import requests
+         
+baseUrl = 'https://api.extremecloudiq.com'
+parent_id = 'Parent ID'
+floor_id = 'Floor ID '
+access_token = '***'
+
+url = f"{baseUrl}/essentials/eloc/clients/location/current-of-all-on-floor"
+headers = {'Authorization': f'Bearer {access_token}'}
+params = {'parentId': f'{parent_id}', 'floorId': f'{floor_id}'}
+
+
+
+response = requests.get(url, headers=headers, params=params)
+
+print("Status Code:", response.status_code)
+
+content_type = response.headers.get('Content-Type')
+if content_type and 'application/json' in content_type:
+    try:
+        print("Response Body:", response.json())
+    except ValueError:
+        print("Response is not valid JSON")
+else:
+    print("Content-Type is not application/json")
+    print(response.text)
---
+++ XIQ API Python collection/Essentials - ExtremeLocation/Get the last known location of the client in the specified interval.py
@@ -0,0 +1,27 @@
+import requests
+         
+baseUrl = 'https://api.extremecloudiq.com'
+parent_id = 'Parent ID'
+floor_id = 'Floor ID '
+client_mac = 'Client MAC address'
+access_token = '***'
+
+url = f"{baseUrl}/essentials/eloc/clients/location/last-known-location-in-interval-on-floor"
+headers = {'Authorization': f'Bearer {access_token}'}
+params = {'parentId': f'{parent_id}', 'floorId': f'{floor_id}', 'clientMac': f'{client_mac}', 'interval': 'THIRTY_DAYS'}
+
+
+
+response = requests.get(url, headers=headers, params=params)
+
+print("Status Code:", response.status_code)
+
+content_type = response.headers.get('Content-Type')
+if content_type and 'application/json' in content_type:
+    try:
+        print("Response Body:", response.json())
+    except ValueError:
+        print("Response is not valid JSON")
+else:
+    print("Content-Type is not application/json")
+    print(response.text)
